72ad606e5c2b036c69ebdfa1902dc00b88d938f6
discoduro <david.diaz637@misena.edu.co>
2023-05-30 09:39:51 -0500

    chore: prueba

--- library/configServer.php
+++ library/configServer.php
@@ -1,5 +1,11 @@
 <?php
-define("SERVER", "srvdreamme.mysql.database.azure.com");
+define("SERVER", "bdsrvdream.mysql.database.azure.com");
 define("USER", "Administrador");
 define("BD", "store");
 define("PASS", "Azure.comsrv");
+$conn = new mysqli($host, $usuario, $contrasenia, $base_de_datos);
+if ($conn->connect_errno) {
+    echo "Falló la conexión a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}
+
+
