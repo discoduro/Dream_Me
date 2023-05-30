69b5cb0c6f9b12da77f8dd9e18d06369aacf455f
discoduro <david.diaz637@misena.edu.co>
2023-05-30 08:16:42 -0500

    chore: prueba

--- library/consulSQL.php
+++ library/consulSQL.php
@@ -2,16 +2,21 @@
 /* Clase para ejecutar las consultas a la Base de Datos*/
 class ejecutarSQL {
     public static function conectar(){           
-            $conn = new mysqli($host, $usuario, $contrasenia, $base_de_datos);
-            $conn = mysqli_connect(SERVER, USER, PASS, BD);
+        if(!$conn =  mysqli_real_connect(SERVER,USER,BD,PASS)){
+            $conn = mysqli_init();
+          mysqli_ssl_set($conn,NULL,NULL, "assets/img/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+          mysqli_real_connect($conn, 'bdsrvdream.mysql.database.azure.com', 'Administrador', 'Azure.comsrv', 'store', MYSQLI_CLIENT_SSL);
+          if (mysqli_connect_errno()) {
+          die('Failed to connect to MySQL: '.mysqli_connect_error());
+          }
+          }
+  
+  
+          /* Codificar la información de la base de datos a UTF8*/
+          mysqli_set_charset($conn, "utf8");
+          return $conn;  
+      }
 
-                if ($conn->connect_errno) {
-             echo "Falló la conexión a MySQL: (" . $conn->connect_errno . ") " . $conn->connect_error;
-}
-        /* Codificar la información de la base de datos a UTF8*/
-        mysqli_set_charset($conn, "utf8");
-        return $conn;  
-    }
     public static function consultar($query) {
         if (!$consul = mysqli_query(ejecutarSQL::conectar(), $query)) {
             echo 'Error en la consulta SQL ejecutada';
