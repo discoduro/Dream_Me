ece4fa4eb59d88ee7e6573a232049d264c5cb34d
david alejandro diaz quimbaya <88217127+discoduro@users.noreply.github.com>
2023-06-01 02:08:09 +0000

    prueba

--- library/consulSQL.php
+++ library/consulSQL.php
@@ -5,6 +5,8 @@
             $conn = mysqli_init();
             mysqli_ssl_set($conn,NULL,NULL, "/assets/img/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
             mysqli_real_connect($conn, 'srvdreamme02.mysql.database.azure.com', 'useradmin', 'dbdream.01', 'store', 33062, MYSQLI_CLIENT_SSL);
+            PRINT_R($conn); 
+            DIE('XXXXX');
             if (mysqli_connect_errno()) {
             die('Failed to connect to MySQL: '.mysqli_connect_error());
             }
