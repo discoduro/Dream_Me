1761867cd871a9b31e53254f208240e4218c4e02
david alejandro diaz quimbaya <88217127+discoduro@users.noreply.github.com>
2023-06-01 15:38:20 +0000

    prueba

--- library/consulSQL.php
+++ library/consulSQL.php
@@ -4,13 +4,13 @@ class ejecutarSQL {
     public static function conectar(){
         $conn = mysqli_init();
         mysqli_ssl_set($conn,NULL,NULL, "/assets/img/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
-        // mysqli_real_connect($conn, 'mydemoserver.mysql.database.azure.com', 'myadmin', 'yourpassword', 'quickstartdb', 3306, MYSQLI_CLIENT_SSL);
+        mysqli_real_connect($conn, 'srvdreamme02.mysql.database.azure.com', 'useradmin', 'bddreamme.1', 'quickstartdb', 3306, MYSQLI_CLIENT_SSL);
         if(!$conn=  mysqli_connect(SERVER,USER,PASS,BD)){
             echo "Error en el servidor, verifique sus datos";
         }
         /* Codificar la información de la base de datos a UTF8*/
-        mysqli_set_charset($con, "utf8");
-        return $con;  
+        mysqli_set_charset($conn, "utf8");
+        return $conn;  
     }
     public static function consultar($query) {
         if (!$consul = mysqli_query(ejecutarSQL::conectar(), $query)) {
