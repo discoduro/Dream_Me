53171bf60c16e0e5d61fe49da59164d9d92908f6
david alejandro diaz quimbaya <88217127+discoduro@users.noreply.github.com>
2023-06-01 16:09:24 +0000

    prueba

--- library/configServer.php
+++ library/configServer.php
@@ -1,6 +1,6 @@
 <?php
 define("SERVER", "localhost");
-define("USER", "id20768935_srvdreame");
-define("PASS", "Websrvdream2023*");
-define("BD", "id20768935_store");
+define("USER", "root");
+define("PASS", "");
+define("BD", "store");
 
